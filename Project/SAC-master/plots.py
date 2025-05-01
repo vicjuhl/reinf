08b058053d29aa4c587b8fc13406bf08fccf6fff
vicjuhl <victorkjellerupjuhl@me.com>
2025-05-01 09:14:42 +0200

    more plots wrapping

--- Project/SAC-master/plots.py
+++ Project/SAC-master/plots.py
@@ -4,7 +4,7 @@ from config import RESULTS_DIR, PLOTS_DIR
 import json
 import re
 
-def add_algorithm_results_to_plot(system_name, algorithm, epsd_steps, color='blue'):
+def add_algorithm_results_to_plot(system_name, algorithm, epsd_steps):
     # Load JSON results
     with (RESULTS_DIR / f"mean_rewards_{system_name}_{epsd_steps}_5.json").open('r') as f:
         rewards = json.load(f)
@@ -21,19 +21,22 @@ def add_algorithm_results_to_plot(system_name, algorithm, epsd_steps, color='blu
     time_steps = np.arange(len(mean_values))
 
     # Add to the plot
-    plt.plot(time_steps * epsd_steps, mean_values, color=color, label=algorithm)
-    plt.fill_between(time_steps * epsd_steps, min_values, max_values, color=color, alpha=0.2)
+    plt.plot(time_steps * epsd_steps, mean_values, label=algorithm)
+    plt.fill_between(time_steps * epsd_steps, min_values, max_values, alpha=0.2)
 
-system_name = "Pendulum-v1"
-plt.figure(figsize=(10, 6))
-add_algorithm_results_to_plot(system_name, "SAC", 200, color='blue')
+def plot_system(system_name, algs, epsd_steps):
+    plt.figure(figsize=(10, 6))
+    for alg in algs:
+        add_algorithm_results_to_plot(system_name, alg, epsd_steps)
 
-# Add plot labels and title
-plt.xlabel('Time steps')
-plt.ylabel('Episode average reward')
-plt.title(system_name)
-plt.legend()
+    # Add plot labels and title
+    plt.xlabel('Time steps')
+    plt.ylabel('Episode average reward')
+    plt.title(system_name)
+    plt.legend()
 
-# Save the plot
-plt.savefig(PLOTS_DIR / f'{system_name}_rewards_plot.png', dpi=300, bbox_inches='tight')
-plt.close()
+    # Save the plot
+    plt.savefig(PLOTS_DIR / f'{system_name}_rewards_plot.png', dpi=300, bbox_inches='tight')
+    plt.close()
+
+plot_system("Pendulum-v1", ["SAC"], 200)
