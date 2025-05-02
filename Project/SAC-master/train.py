a7545a21f106a37112aa50775e9c7a09b37c6445
vicjuhl <victorkjellerupjuhl@me.com>
2025-05-02 11:54:42 +0200

    Writing json results before joining processes

--- Project/SAC-master/train.py
+++ Project/SAC-master/train.py
@@ -17,14 +17,16 @@ def run_env(proc_id, alg, system_type, total_steps, epsd_steps, result_queue):
         alg=alg,
         reward_scale=REWARD_SCALE[system_type],
         epsd_steps=epsd_steps,
-        video_freq=total_steps // 5, # TODO: will this begin videos in the middle of episodes?
+        video_freq=None,#total_steps // 5, # TODO: will this begin videos in the middle of episodes?
         proc_id=proc_id
     )
     results = system.train_agent(total_steps)
     # Save model to models directory
     model_path = MODELS_DIR / f"{system_type}_{alg}_{proc_id}.p"
     pickle.dump(system.agent, open(model_path, 'wb'))
+    print(f"[Proc {proc_id}] Finished training", flush=True)
     result_queue.put((proc_id, results))
+    print(f"[Proc {proc_id}] Results put to queue", flush=True)
 
 if __name__ == "__main__":
     mp.set_start_method("spawn")
@@ -46,17 +48,18 @@ if __name__ == "__main__":
         p.start()
         processes.append(p)
 
-    for p in processes:
-        p.join()
-
-    # Collect and save rewards
+    # Collect and save rewards as they arrive
     all_results = [None] * n_test
-    while not result_queue.empty():
-        proc_id, results = result_queue.get()
+    for _ in range(n_test):
+        proc_id, results = result_queue.get()  # Will block until a next result is available
         all_results[proc_id] = results
 
-
     # Save to JSON in results directory
-    results_path = RESULTS_DIR / f'results_{system_type}_{alg}_{total_steps}_{n_test}.json'
+    results_path = RESULTS_DIR / f'results_{system_type}_{alg}_{int(total_steps)}_{n_test}.json'
     with open(results_path, 'w') as f:
         json.dump(all_results, f, indent=2)
+        print("Written results to json (safe to interrupt if needed)")
+        
+    for p in processes:
+        p.join()
+    print("Joined processes successfully")
