a6fa05a2999dd1b882ac4833b3ae1f309dd1e2e4
vicjuhl <victorkjellerupjuhl@me.com>
2025-05-02 11:52:04 +0200

    plot episode length

--- Project/SAC-master/plots.py
+++ Project/SAC-master/plots.py
@@ -4,7 +4,7 @@ from config import RESULTS_DIR, PLOTS_DIR
 import json
 import re
 
-def add_algorithm_results_to_plot(system_name, alg, total_steps):
+def add_alg_results_to_plot(system_name, alg, total_steps):
     # Load JSON results
     with (RESULTS_DIR / f"results_{system_name}_{alg}_{total_steps}_5.json").open('r') as f:
         results = json.load(f)
@@ -22,10 +22,8 @@ def add_algorithm_results_to_plot(system_name, alg, total_steps):
     # min_values = np.min(rewards, axis=0)
     # max_values = np.max(rewards, axis=0)
 
-    # Create time steps
     # time_steps = np.arange(len(mean_values))
 
-    # Add to the plot
     for proc_id in range(len(results)):
         r = total_rewards[proc_id]
         es = epsd_steps_actual[proc_id]
@@ -33,10 +31,25 @@ def add_algorithm_results_to_plot(system_name, alg, total_steps):
         plt.plot(acc_steps, r, label=alg)
         # plt.fill_between(time_steps * epsd_steps, min_values, max_values, alpha=0.2)
 
+def add_epsd_len_to_plot(system_name, alg, total_steps):
+    # Load JSON results
+    with (RESULTS_DIR / f"results_{system_name}_{alg}_{total_steps}_5.json").open('r') as f:
+        results = json.load(f)
+
+    n_proc = len(results)
+    epsd_steps_actual = [[] for _ in range(n_proc)]
+    for proc_id, proc_results in enumerate(results):
+        for epsd_results in proc_results:
+            epsd_steps_actual[proc_id].append(epsd_results["final_step"] + 1)
+
+    for proc_id in range(len(results)):
+        plt.plot(epsd_steps_actual[proc_id], label=alg)
+
 def plot_system(system_name, algs, total_steps):
+    # REWARDS
     plt.figure(figsize=(10, 6))
     for alg in algs:
-        add_algorithm_results_to_plot(system_name, alg, total_steps)
+        add_alg_results_to_plot(system_name, alg, total_steps)
 
     # Add plot labels and title
     plt.xlabel('Time steps')
@@ -48,5 +61,20 @@ def plot_system(system_name, algs, total_steps):
     plt.savefig(PLOTS_DIR / f'{system_name}_rewards_plot.png', dpi=300, bbox_inches='tight')
     plt.close()
 
+    # EPISODE LENGTH
+    plt.figure(figsize=(10, 6))
+    for alg in algs:
+        add_epsd_len_to_plot(system_name, alg, total_steps)
+    
+    # Add plot labels and title
+    plt.xlabel('Episode')
+    plt.ylabel('Episode length')
+    plt.title(system_name)
+    plt.legend()
+
+    # Save the plot
+    plt.savefig(PLOTS_DIR / f'{system_name}_epsd_len_plot.png', dpi=300, bbox_inches='tight')
+    plt.close()
+
 # plot_system("Pendulum-v1", ["SAC", "SAC2"], 200)
-plot_system("Hopper-v4", ["SAC"], 1000)
+plot_system("Hopper-v4", ["SAC"], int(1e4))
